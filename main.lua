e71d60a4bdd5f163abf3695ce741d06809ee8abe
ferjpi <fernandojpt1203@gmail.com>
2025-10-04 11:10:48 -0500

    feat: draw player sprite

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 function love.load()
-
+  Sprites = {}
+  Sprites.ship = love.graphics.newImage("sprites/ship.png")
 end
 
 function love.update(dt)
@@ -8,5 +9,7 @@ end
 
 function love.draw()
   love.graphics.setFont(love.graphics.newFont(64))
-  love.graphics.printf("Asteroids", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+  -- love.graphics.printf("Asteroids", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+  
+  love.graphics.draw(Sprites.ship, love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
 end
