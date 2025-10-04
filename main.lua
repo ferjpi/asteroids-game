0826a0f8f683c5de1ad6f522088f4042a11f282e
ferjpi <fernandojpt1203@gmail.com>
2025-10-04 12:36:17 -0500

    fix: rotation offset, ship movement

--- main.lua
+++ main.lua
@@ -4,6 +4,9 @@ function love.load()
 
   ship = { x= love.graphics.getWidth() / 2, y = love.graphics.getHeight() / 2, rotate = 0 }
   ship.speed = 100
+  ship.rotateSpeed = 180
+
+  rotationOffset = 90
 end
 
 function love.update(dt)
@@ -16,6 +19,13 @@ function love.draw()
   -- love.graphics.printf("Asteroids", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
   
   love.graphics.draw(Sprites.ship, ship.x, ship.y, math.rad(ship.rotate), 1, 1, Sprites.ship:getWidth() / 2, Sprites.ship:getHeight() / 2)
+   -- debug: draw forward vector so you can see direction
+  local angleForMovement = math.rad(ship.rotate - 90) -- offset
+  local fx = ship.x + math.cos(angleForMovement) * 40
+  local fy = ship.y + math.sin(angleForMovement) * 40
+  love.graphics.setColor(1, 0, 0) -- red
+  love.graphics.line(ship.x, ship.y, fx, fy)
+  love.graphics.setColor(1, 1, 1) -- reset
 end
 
 function love.keypressed(key)
@@ -29,14 +39,14 @@ function Move_player( dt)
   local isScancode = love.keyboard.isScancodeDown
   if isScancode("a") then
     -- rotate left
-    ship.rotate = ship.rotate - 180 * dt
+    ship.rotate = ship.rotate - ship.rotateSpeed * dt
   elseif isScancode("d") then
     -- move right
-    ship.rotate = ship.rotate + 180 * dt
+    ship.rotate = ship.rotate + ship.rotateSpeed * dt
   end
 
     -- Move forward/backward using vector math
-  local angle = math.rad(ship.rotate)
+  local angle = math.rad(ship.rotate - rotationOffset)
   if isScancode("w") then
     ship.x = ship.x + math.cos(angle) * ship.speed * dt
     ship.y = ship.y + math.sin(angle) * ship.speed * dt
